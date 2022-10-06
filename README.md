78b36793f25cf6db2720854d169d3cae05c28e82
Yasmin Bustami <yasminbustami@Yasmins-MacBook-Pro.local>
2022-10-05 23:55:52 -0500

    redid functions, naming functions, etc.

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Challenge5
 5th challenge of class
 LIVE LINK:  https://yazbustami.github.io/Challenge5/
-SC: <img width="1323" alt="WDPSC" src="https://user-images.githubusercontent.com/111784041/193444639-2e774b69-4548-4e90-aa85-0ec7c4235bc3.png">
+SC: <img width="1321" alt="SC UPDATED" src="https://user-images.githubusercontent.com/111784041/194217466-d9bb5212-9c78-469d-97a5-4275f452c1ee.png">
 
 The purpose of this weeks challenge was to create a day planner and apply what we learned this week to something practical us, ourselves may even use. Attached screenshot of work.
 
 ##coded by Yasmin Bustami
+##Was told to work on again, per Professor Request October 4th, 2022.
--- assets/script.js
+++ assets/script.js
@@ -3,44 +3,42 @@ $(document).ready(function () {
     // using moment.js for Date/Time
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
- // adding an event listener for all save buttons
-// save data to local storage
+ // adding an event listener for all save buttons to save data to local storage
     $(".saveBtn").on("click", function() {
-        console.log(this);
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-        localStorage.setItem(time, text);
+        // THIS INITIALIZES A VARIABLE
+        let Block_period = $(this).siblings(".description").val();
+        let Material = $(this).parent().attr("id");
+        localStorage.setItem(Material, Block_period);
     })
 
 // local storage
-$("#hour7 .description").val(localStorage.getItem("hour7"));
-$("#hour8 .description").val(localStorage.getItem("hour8"));
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#7oclock .description").val(localStorage.getItem("7oclock"));
+$("#8oclock .description").val(localStorage.getItem("8oclock"));
+$("#9oclock .description").val(localStorage.getItem("9oclock"));
+$("#10oclock .description").val(localStorage.getItem("10oclock"));
+$("#11oclock .description").val(localStorage.getItem("11oclock"));
+$("#12oclock .description").val(localStorage.getItem("12oclock"));
+$("#1oclock .description").val(localStorage.getItem("1oclock"));
+$("#2oclock .description").val(localStorage.getItem("2oclock"));
+$("#3oclock .description").val(localStorage.getItem("3oclock"));
 
 // using moment.js for Date/Time
-function hourTracker() {
-    var currentHour = moment().hour(); 
+function PeriodNote() {
+    let Now = moment().hour(); 
 
 
 // creating for loop to go through each block time
 // if statement to add color
 
     $(".time-block").each(function () {
-        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-        console.log( blockHour, currentHour)
-
-        if (blockHour < currentHour) {
+        let TimeFrame = parseInt($(this).attr("id").split("hour")[1]);
+        // console.log(TimeFrame, Now)
+        if (TimeFrame < Now) {
             $(this).addClass("past");
             $(this).removeClass("future");
             $(this).removeClass("present");
         }
-        else if (blockHour === currentHour) {
+        else if (TimeFrame === Now) {
             $(this).removeClass("past");
             $(this).addClass("present");
             $(this).removeClass("future");
@@ -51,8 +49,5 @@ function hourTracker() {
             $(this).removeClass("past");
             $(this).addClass("future");
         }
-    })
-}
-
- hourTracker();
-})
+    })}
+ PeriodNote();})
